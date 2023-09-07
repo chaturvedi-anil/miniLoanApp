ac9dfc0571041a5d509bc2b7fe49a7064aab456c
chaturvedi-anil <chaturvedia560@gmail.com>
2023-09-07 17:14:05 +0530

    setup css for signin and singup page

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,3 +1,4 @@
+import Admin from '../models/admin.js';
 
 export function signIn(req, res)
 {
@@ -10,4 +11,41 @@ export function signUp(req, res)
     return res.render('admin_SignUp',{
         title: 'Admin SignUp'
     });
+}
+
+// creating new users
+export async function createAdmin(req, res)
+{
+    try
+    {
+        // console.log(req.body);
+        let admin = await Admin.findOne({email: req.body.email});
+
+        if(!admin)
+        {
+            if(req.body.password !== req.body.confirm_password)
+            {
+                req.flash('error', 'Password and confirm password should be same');
+                return res.redirect('back');
+            }
+            else
+            {
+                let newAdmin = await Admin.create(req.body);
+                
+                req.flash('success', 'New Admin registered');
+                return res.redirect('/admin/sign-in');
+            }
+        }
+        else
+        {
+            // console.log('user is already registered');
+            req.flash('error', 'This Admin is already registered');
+            return res.redirect('back');
+        }
+    }
+    catch(err)
+    {
+        console.log('Error in creating admin :', err);
+        return res.send('Internal Server Error');
+    }
 }
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { signIn, signUp } from '../controllers/adminControllers.js';
+import { signIn, signUp, createAdmin } from '../controllers/adminControllers.js';
 const adminRouter = Router();
 
 adminRouter.get('/sign-in', signIn);
 adminRouter.get('/sign-up', signUp);
+
+adminRouter.post('/create', createAdmin);
 
 export default adminRouter;
