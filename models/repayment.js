56cf6ebef89f897649fce81cfdbcf9fea6f168cd
chaturvedi-anil <chaturvedia560@gmail.com>
2023-09-07 15:13:34 +0530

    setup Schema for repayments

---
+++ models/repayment.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const repaymentSchema = new Schema(
+{
+    loan: 
+    { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Loan', 
+        required: true 
+    },
+    amount: 
+    { 
+        type: Number, 
+        required: true 
+    },
+    status: 
+    {
+        type: String,
+        enum: ['PENDING', 'PAID'],
+        default: 'PENDING',
+    },
+    paymentDate: 
+    { 
+        type: Date, 
+        required: true 
+    },
+}, 
+{
+
+    timestamps: true
+});
+
+const Repayment = mongoose.model('Repayment', repaymentSchema);
+
+export default Repayment;
